e54debb14cedc9e4c0f7f66f74dff42c2a11caa0
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-12-09 10:51:15 +0300

    single_responsibility using deal calc

--- SOLID/single_responsibility_principle.rb
+++ SOLID/single_responsibility_principle.rb
@@ -1,5 +1,7 @@
 
-# class that does not obey the single responsibility principle i.e
+# class that does not obey the single responsibility principle i,e
+
+# 
 
 class Student
   attr_accessor :first_term_home_work, :first_term_test, :first_term_paper
@@ -29,4 +31,59 @@ class Grade
 
   def initialize(name,  )
   (home_work + test + paper) / 3
+end
+
+
+# consider the following deal processor class
+
+class DealProcessor
+  def initialize(deals)
+    @deals = deals # an array of deals waiting to be processed
+  end
+
+  def process
+    @deals.each do |deal|
+      Commission.create(deal: deal, amount: calculate_commission)
+      mark_deal_processed
+    end
+  end
+
+
+  def mark_deal_processed
+    @deal.process = true
+    @deal.save!
+  end
+
+  def calculate_commission
+    @deal.dollar_amount * 0.05
+  end
+
+end
+
+# A change to how we calculate the commision would require a change to this class
+
+class DealProcessor
+  def initialize(deals)
+    @deals = deals
+  end
+
+  def process
+    @deals.each do |deal|
+      mark_deal_processed
+      CommissionCalculator.new.create_commission(deal)
+    end
+  end
+
+  private
+
+  def mark_deal_processed
+    @deal.processed = true
+    @deal.save!
+  end
+end
+
+class CommissionCalculator
+  def create_commission(deal)
+    Commission.create(deal: deal, amount: deal.dollar_amount * 0.05)
+  end
 end
