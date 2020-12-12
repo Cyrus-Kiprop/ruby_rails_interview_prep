bcd5f2f34f493c72526467192eb6340440a1aad1
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-12-12 12:07:46 +0300

    super keyword in ruby classes

--- super_keyword.rb
+++ super_keyword.rb
@@ -5,26 +5,26 @@
 
 #=> implicit arguments
 
-# --> When a method with arguments is overriden by one of its child classes then a call 
+# --> When a method with arguments is overriden by one of its child classes then a call
 # --> to super method without any arguments in the child method will automatically pass
 # --> automatically pass the arguments of the child method to the parent method.
 
 # consider
 # case 1
 class Parent
-	def say(message)
-		p message
-	end
+  def say(message)
+    p message
+  end
 end
 
 
 class Child < Parent
 
-	# overrides the parent#say method
-	def say(message)
-		# this invoke the parent#say method found on the ancestor chain and passing the child message variable to it
-		super
-	end
+  # overrides the parent#say method
+  def say(message)
+    # this invoke the parent#say method found on the ancestor chain and passing the child message variable to it
+    super
+  end
 end
 
 
@@ -35,7 +35,52 @@ end
 # case II
 
 class Parent
-	def say
-		p "I am the parent"
-	end
+  def say
+    p "I am the parent"
+  end
 end
+
+
+class Child < Parent
+  def say(message)
+    super
+  end
+
+end
+
+Child.new.say("hello") # => this will throw argument error
+
+
+# to fix this
+
+class Parent
+  def say
+    p 'something'
+  end
+end
+
+class Child < Parent
+  def say(message)
+    super() # invoking the super with no variables to avoid the argument error
+  end
+end
+
+Child.new.say("hello") #=> 'something'
+
+
+# yield keyword
+
+
+class Parent
+  def say
+    yield
+  end
+end
+
+class Child < Parent
+  def say
+  end
+
+end
+
+Child.new.say { p 'I am the parent'} #=> 'I am the parent'
